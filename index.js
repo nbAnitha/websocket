57cde34b219cd8c095fecb5c3968b153d28b96a7
nbAnitha <anitha.nb6@gmail.com>
2016-04-26 16:34:28 -0700

    websocket upload

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by root on 2/17/16.
+ */
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+app.use(express.static(__dirname + "/"))
+var server = http.createServer(app)
+server.listen(port)
+console.log("http server listening on %d", port)
+var wss = new WebSocketServer({server:server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+    console.log("websocket connection open")
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message)
+
+        message = "I am fine Client !";
+        ws.send(message);
+        console.log('message sent: %s', message)
+    });
+
+    ws.on("close", function() {
+        console.log("websocket connection close")
+    })
+})
